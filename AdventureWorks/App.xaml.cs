61adc4ca9de813f05cef2aff1d64463df0f6a828
Denislubapershins <Denislubapershins@gmail.com>
2025-03-06 23:09:38 +0300

    changed StaticResource to DynamicResource

--- AdventureWorks/App.xaml.cs
+++ AdventureWorks/App.xaml.cs
@@ -7,16 +7,6 @@ namespace AdventureWorks
         public App()
         {
             InitializeComponent();
-
-            // Проверка загруженных ресурсов
-            if (Resources.TryGetValue("ApplicationTitle", out var title))
-            {
-                Debug.WriteLine("Resource found: ApplicationTitle");
-            }
-            else
-            {
-                Debug.WriteLine("Resource NOT found: ApplicationTitle");
-            }
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
